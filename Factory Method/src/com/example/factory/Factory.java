612dba1150871c0088029d2720c99aa27657d16c
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-11-06 03:12:20 +0900

    Factory Method

---
+++ Factory Method/src/com/example/factory/Factory.java
@@ -0,0 +1,11 @@
+package com.example.factory;
+
+public abstract class Factory {
+	public final Product create(String serialNumber) {
+		Product product = createProduct(serialNumber);
+		registerProduct(product);
+		return product;
+	}
+	protected abstract Product createProduct(String serialNumber);
+	protected abstract void registerProduct(Product product);
+}
