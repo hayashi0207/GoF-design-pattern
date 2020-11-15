07a5bc973491c801656cdd338f074670069b4f51
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-11-15 23:09:05 +0900

    Facadeパターン完成

---
+++ Facade/src/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+	public static void main(String[] args) {
+		String yamadaAccount = "0001";
+		int yamadaResult = BankManager.takeLoan(yamadaAccount, 1000000);
+		System.out.println();
+		
+		String tanakaAccount = "0002";
+		int tanakaResult = BankManager.takeLoan(tanakaAccount, 1000000);
+	}
+}
