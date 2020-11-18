3ea6a756ce6412deb51f0131e20a211ca2a48819
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-11-18 11:48:32 +0900

    ChainOfResponsibility

---
+++ ChainOfResponsibility/src/com/example/GeneralAffairs.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class GeneralAffairs extends ApprovalPerson{
+	public GeneralAffairs(String name) {
+		super(name);
+	}
+	@Override
+	protected boolean judge(DecisionRequest request) {
+		return false;
+	}
+}
