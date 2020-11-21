36ed51cfb3222cb5e6fadbd01bda47fce41933c5
Hayashi Ryunosuke <hayashi.ryunosuke.mdk@outlook.jp>
2020-11-21 21:51:38 +0900

    Commandパターン完了

---
+++ Command/src/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+	public static void main(String[] args) {
+		String path = "sample.txt";
+		try {
+			MacroCommand before = new MacroCommand();
+			Command fileCheck = new FileCheckCommand(path);
+			Command dbCheck = new DbCheckCommand();
+				before.addCommand(fileCheck);
+				before.addCommand(dbCheck);
+				before.execute();
+				
+				MacroCommand main = new MacroCommand();
+				Command fileWrite = new FileWriteCommand(path);
+				
+				main.addCommand(fileWrite);
+				main.execute();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
